b7f3821ffd087a9ee8195c8826e9d169b763fc5a
duongphihung <hungdarker113@gmail.com>
2024-02-20 10:02:33 +0700

    text

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import second from '../login/'
+
+const Login = () => {
+    return (
+        <div>
+            <img src='./image/background.jpg' alt='background' className='' />
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
